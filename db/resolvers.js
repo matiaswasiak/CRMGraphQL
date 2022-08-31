6fb33a5cb08212510c45b77424db80c133bad4d2
Matias Wasiak <matiaswasiak@gmail.com>
2022-08-31 13:51:50 -0300

    Actualizar un cliente

--- db/resolvers.js
+++ db/resolvers.js
@@ -183,6 +183,25 @@ const resolvers = {
         console.log(error);
       }
     },
+    actualizarCliente: async (_, { id, input }, ctx) => {
+      // Verificar si existe o no
+      let cliente = await Cliente.findById(id);
+
+      if (!cliente) {
+        throw new Error("Ese cliente no existe");
+      }
+
+      // Verificar si el vendedor es quien edita
+      if (cliente.vendedor.toString() !== ctx.usuario.id) {
+        throw new Error("No tienes las credenciales");
+      }
+
+      // Guardar el cliente
+      cliente = await Cliente.findOneAndUpdate({ _id: id }, input, {
+        new: true,
+      });
+      return cliente;
+    },
   },
 };
 
--- db/schema.js
+++ db/schema.js
@@ -84,6 +84,7 @@ const typeDefs = gql`
 
     # Clientes
     nuevoCliente(input: ClienteInput): Cliente
+    actualizarCliente(id: ID!, input: ClienteInput): Cliente
   }
 `;
 
