c759be83e853ac0d3f35e7dd7f71cb23fc2fdb06
Matias Wasiak <matiaswasiak@gmail.com>
2022-08-30 23:38:22 -0300

    first commit

---
+++ db/resolvers.js
@@ -0,0 +1,164 @@
+const Usuario = require("../models/Usuario");
+const Producto = require("../models/Producto");
+const Cliente = require("../models/Cliente");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
+
+const crearToken = (usuario, secreta, expiresIn) => {
+  console.log(usuario);
+  const { id, email, nombre, apellido } = usuario;
+
+  return jwt.sign({ id, email, nombre, apellido }, secreta, { expiresIn });
+};
+
+// Resolvers
+const resolvers = {
+  Query: {
+    obtenerUsuario: async (_, { token }) => {
+      const usuarioId = await jwt.verify(token, process.env.SECRETA);
+
+      return usuarioId;
+    },
+    obtenerProductos: async () => {
+      try {
+        const productos = await Producto.find({});
+        return productos;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    obtenerProducto: async (_, { id }) => {
+      // Revisar si el producto existe o no
+      const producto = await Producto.findById(id);
+
+      if (!producto) {
+        throw new Error("Producto no encontrado");
+      }
+
+      return producto;
+    },
+    obtenerClientes: async () => {
+      try {
+        const clientes = await Cliente.find({});
+        return clientes;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  Mutation: {
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      // Revisar el el usuario ya esta registrado
+      const existeUsuario = await Usuario.findOne({ email });
+      if (existeUsuario) {
+        throw new Error("El usuario ya esta registrado");
+      }
+
+      // Hashear su password
+      const salt = bcryptjs.genSaltSync(10);
+      input.password = bcryptjs.hashSync(password, salt);
+
+      try {
+        // Guardarlo en la base de datos
+        const usuario = new Usuario(input);
+        usuario.save(); // guardarlo
+        return usuario;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    autenticarUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      // Si el usaurio existe
+      const existeUsuario = await Usuario.findOne({ email });
+      if (!existeUsuario) {
+        throw new Error("El usuario no esta registrado");
+      }
+
+      // Revisar si el password es correcto
+      const passwordCorrecto = await bcryptjs.compare(
+        password,
+        existeUsuario.password
+      );
+      if (!passwordCorrecto) {
+        throw new Error("El password es incorrecto");
+      }
+
+      // Crear el token
+      return {
+        token: crearToken(existeUsuario, process.env.SECRETA, "24h"),
+      };
+    },
+    nuevoProducto: async (_, { input }) => {
+      try {
+        const producto = new Producto(input);
+
+        // Almacenar en la db
+        const resultado = await producto.save();
+
+        return resultado;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    actualizarProducto: async (_, { id, input }) => {
+      // Revisar si el producto existe o no
+      let producto = await Producto.findById(id);
+
+      if (!producto) {
+        throw new Error("Producto no encontrado");
+      }
+
+      // Guardarlo en la base de datos
+      producto = await Producto.findOneAndUpdate({ _id: id }, input, {
+        new: true,
+      });
+
+      return producto;
+    },
+    eliminarProducto: async (_, { id }) => {
+      // Revisar si el producto existe o no
+      let producto = await Producto.findById(id);
+
+      if (!producto) {
+        throw new Error("Producto no encontrado");
+      }
+
+      // Eliminar
+      await Producto.findOneAndDelete({ _id: id });
+
+      return "Producto Eliminado";
+    },
+    nuevoCliente: async (_, { input }, ctx) => {
+      console.log(ctx);
+
+      const { email } = input;
+      // Verificar si el cliente ya esta registrado
+      console.log(input);
+
+      const cliente = await Cliente.findOne({ email });
+      if (cliente) {
+        throw new Error("Ese cliente ya esta registrado");
+      }
+
+      const nuevoCliente = new Cliente(input);
+
+      // Asignar el vendedor
+      nuevoCliente.vendedor = "630eb0c092f996599b76c0b8";
+
+      // Guardarlo en la db
+      try {
+        const resultado = await nuevoCliente.save();
+        return resultado;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ index.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./db/schema");
+const resolvers = require("./db/resolvers");
+const conectarDB = require("./config/db");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
+
+// Conectar a la base de datos
+conectarDB();
+
+// Servidor
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    // console.log(req.headers["authorization"]);
+
+    const token = req.headers["authorization"] || "";
+    if (token) {
+      try {
+        const usuario = jwt.verify(token, process.env.SECRETA);
+
+        console.log(usuario);
+
+        return {
+          usuario,
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+});
+
+// Arrancar el servidor
+server.listen().then(({ url }) => {
+  console.log(`Servidor listo en la URL ${url}`);
+});
